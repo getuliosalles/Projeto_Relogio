c2c39feb8bac860e6349acb60440f3bbeac3536d
Getúlio Salles <67875077+getuliosalles@users.noreply.github.com>
2020-07-30 20:01:57 -0300

    function add_zero

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-var inicio = false, hora_inicio, init_cronom, hora_atual, time_past, hora, minut, seg, miliseg
+var inicio = false, hora_inicio, init_cronom, hora_atual, time_past, hora, minut, seg, miliseg, h, m, s, milis
 
 document.getElementById('comecar_parar').onclick = function(){
+
+    function add_zero(number){
+        if(number < 10){
+            return '0'+`${number}`
+        }else{
+            return `${number}`
+        }
+    }
 
     if(!inicio){
         inicio = true
@@ -16,20 +24,22 @@ document.getElementById('comecar_parar').onclick = function(){
             hora_atual = new Date().getTime()
             time_past = hora_atual - hora_inicio
 
-            hora = Math.floor(time_past / 3600000)
+           h = add_zero(hora = Math.floor(time_past / 3600000))
             resto = time_past - (hora * 3600000)
 
-            minut = Math.floor(resto / 60000)
+           m = add_zero(minut = Math.floor(resto / 60000))
             resto -= (minut * 60000)
 
-            seg = Math.floor(resto / 1000)
+           s = add_zero(seg = Math.floor(resto / 1000))
             resto -= (seg * 1000)
 
-            miliseg = resto
+           milis = add_zero(miliseg = resto)
+
+            
 
 
 
-            document.getElementById('cronometro').innerHTML = time_past
+            document.getElementById('cronometro').innerHTML = `${h}:${m}:${s}  ${milis}`
 
 
         },10)
@@ -44,5 +54,5 @@ document.getElementById('comecar_parar').onclick = function(){
 document.getElementById('zerar').onclick = function(){
     time_past = 0
     hora_inicio = new Date().getTime()
-    document.getElementById('cronometro').innerHTML = time_past
+    document.getElementById('cronometro').innerHTML = "00:00:00  000"
 }
